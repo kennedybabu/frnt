6b8b083af26a26c0c31751624f9a9a8fc6141e25
kennedybabu <kennedynjogu8@gmail.com>
2025-06-30 06:16:05 +0300

    ft: add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-
+import { ButtonModule } from "primeng/button"
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [RouterOutlet, ProductListComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
