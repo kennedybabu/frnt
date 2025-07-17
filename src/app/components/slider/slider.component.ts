9e1770a19aaa9b0e3d315d235fd6babbcf41b1c7
kennedybabu <kennedynjogu8@gmail.com>
2025-07-17 15:20:19 +0300

    ft: style the hero section

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -2,10 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductCategory } from '../../common/product-category';
 import { ProductService } from '../../services/product.service';
 import { RouterModule } from '@angular/router';
+import { CarouselModule } from 'primeng/carousel';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-slider',
-  imports: [RouterModule],
+  imports: [RouterModule, CarouselModule, CommonModule, ButtonModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
@@ -14,16 +18,45 @@ export class SliderComponent implements OnInit {
   productCategories!: ProductCategory[];
 
   productService = inject(ProductService)
+  responsiveOptions: any[] | undefined;
+
+  carouselItems = [
+    { image: 'images/bg/teal_bg.jpg', alt: 'Teal background' , text: 'Secure Payment'},
+    { image: 'images/bg/pink_bg.jpg', alt: 'Pink background' , text: 'Browse, Safely & Shop'},
+  ];
+
 
   ngOnInit(): void {
     this.listProductCategories();
+
+    this.responsiveOptions = [
+      {
+          breakpoint: '1400px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '1199px',
+          numVisible: 3,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '575px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ]
   }
 
 
   listProductCategories() {
     this.productService.getProductCategories().subscribe(
       data => {
-        console.log('product categories = ' + JSON.stringify(data));
         this.productCategories = data;
       }
     )
