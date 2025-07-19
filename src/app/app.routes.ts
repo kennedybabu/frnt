deaf54ef50c8de0a503c780011395beb852cd728
kennedybabu <kennedynjogu8@gmail.com>
2025-07-20 02:52:00 +0300

    ft: add protected routes with okta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,12 @@ import myAppConfig from './config/my-app-config';
 
 import {
     OKTA_CONFIG,
+    OktaAuthGuard,
     OktaAuthModule,
     OktaCallbackComponent
 } from "@okta/okta-angular";
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 
 export const oktaConfig = Object.assign({
@@ -24,6 +26,9 @@ export const oktaConfig = Object.assign({
 }, myAppConfig.oidc) 
 
 export const routes: Routes = [
+    {
+        path:'members', component: MembersPageComponent, canActivate: [OktaAuthGuard]
+    },
     {
         path:'login/callback', component: OktaCallbackComponent
     },
